b05abfa5431663456fd096b0acb925efe69f0dd8
Luma <lumagoesmontes@gmail.com>
2020-05-29 20:26:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".btn-1").addEventListener("click", function () {
+    document.getElementById("verMais1").classList.toggle('txt-hidden')})
+
+    document.querySelector(".btn-2").addEventListener("click", function () {
+        document.getElementById("verMais2").classList.toggle('txt-hidden')})

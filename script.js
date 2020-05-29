a98546397f7110135e2ef469abdf5dc34cb49746
Luma <lumagoesmontes@gmail.com>
2020-05-29 20:37:52 -0300

    conteúdo

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.querySelector(".btn-1").addEventListener("click", function () {
 
     document.querySelector(".btn-2").addEventListener("click", function () {
         document.getElementById("verMais2").classList.toggle('txt-hidden')})
+
+        document.querySelector(".btn-3").addEventListener("click", function () {
+            document.getElementById("verMais3").classList.toggle('txt-hidden')})

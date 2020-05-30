724a3716cc633324191ad72c21d2f0071d037834
Luma <lumagoesmontes@gmail.com>
2020-05-29 21:20:44 -0300

    conteudo sobre o curso finalizado \o/

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.querySelector(".btn-1").addEventListener("click", function () {
-    document.getElementById("verMais1").classList.toggle('txt-hidden')})
+    document.getElementById("verMais1").classList.toggle('txt-hidden')
+})
 
-    document.querySelector(".btn-2").addEventListener("click", function () {
-        document.getElementById("verMais2").classList.toggle('txt-hidden')})
+document.querySelector(".btn-2").addEventListener("click", function () {
+    document.getElementById("verMais2").classList.toggle('txt-hidden')
+})
 
-        document.querySelector(".btn-3").addEventListener("click", function () {
-            document.getElementById("verMais3").classList.toggle('txt-hidden')})
+document.querySelector(".btn-3").addEventListener("click", function () {
+    document.getElementById("verMais3").classList.toggle('txt-hidden')
+})
+document.querySelector(".btn-4").addEventListener("click", function () {
+    document.getElementById("verMais4").classList.toggle('txt-hidden')
+})
